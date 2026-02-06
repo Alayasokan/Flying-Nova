baa0b66669272ca04cdaf5989235c490a60d6ee9
Alay Ashokan <alayasokan8848@gmail.com>
2026-02-06 12:20:56 +0530

    Jumping resolved

--- script.js
+++ script.js
@@ -1,80 +1,139 @@
+// Elements
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
+var scoreDisplay = document.getElementById("score");
+
+// Game Variables
 var jumping = 0;
 var counter = 0;
-document.getElementById("score").innerHTML = "score : " + counter
 
-hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random()*300)+150);
+scoreDisplay.innerHTML = "Score : " + counter;
+
+
+// Random hole position + score update
+hole.addEventListener("animationiteration", function () {
+    var random = -((Math.random() * 300) + 150);
     hole.style.top = random + "px";
+
     counter++;
-    document.getElementById("score").innerHTML = "score : " + counter
+    scoreDisplay.innerHTML = "Score : " + counter;
 });
 
-setInterval(function(){
+
+// Gravity + Collision detection
+setInterval(function () {
+
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    if(jumping==0){
-        character.style.top = (characterTop+3)+"px";
+
+    // Gravity
+    if (jumping == 0) {
+        character.style.top = (characterTop + 3) + "px";
     }
+
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var cTop = -(500-characterTop);
-    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
-        customAlert.alert('Your maximum score ' + counter,'Game Over')
-        character.style.top = 100 + "px";
-        counter=0;
-    }
-},10);
 
-function jump(){
+    var cTop = -(500 - characterTop);
+
+    // Collision Check
+    if (
+        (characterTop > 480) ||
+        ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))
+    ) {
+        customAlert.alert("Your maximum score " + counter, "Game Over");
+
+        character.style.top = "100px";
+        counter = 0;
+        scoreDisplay.innerHTML = "Score : " + counter;
+    }
+
+}, 10);
+
+
+// Jump Function
+function jump() {
+
+    if (jumping == 1) return;   // Prevent multi jump
+
     jumping = 1;
     let jumpCount = 0;
-    var jumpInterval = setInterval(function(){
+
+    var jumpInterval = setInterval(function () {
+
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if((characterTop>6)&&(jumpCount<15)){
-            character.style.top = (characterTop-5)+"px";
+
+        if ((characterTop > 6) && (jumpCount < 15)) {
+            character.style.top = (characterTop - 5) + "px";
         }
-        if(jumpCount>20){
+
+        if (jumpCount > 20) {
             clearInterval(jumpInterval);
-            jumping=0;
-            jumpCount=0;
+            jumping = 0;
+            jumpCount = 0;
         }
+
         jumpCount++;
-    },10);
+
+    }, 10);
 }
 
-function CustomAlert(){
-    this.alert = function(message,title){
-      document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
-  
-      let dialogoverlay = document.getElementById('dialogoverlay');
-      let dialogbox = document.getElementById('dialogbox');
-      
-      let winH = window.innerHeight;
-      dialogoverlay.style.height = winH+"px";
-      
-      dialogbox.style.top = "100px";
-  
-      dialogoverlay.style.display = "block";
-      dialogbox.style.display = "block";
-      
-      document.getElementById('dialogboxhead').style.display = 'block';
-  
-      if(typeof title === 'undefined') {
-        document.getElementById('dialogboxhead').style.display = 'none';
-      } else {
-        document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
-      }
-      document.getElementById('dialogboxbody').innerHTML = message;
-      document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+
+// Controls
+document.addEventListener("click", jump);
+
+document.addEventListener("keydown", function (e) {
+    if (e.code === "Space" || e.code === "ArrowUp") {
+        jump();
     }
-    
-    this.ok = function(){
-      document.getElementById('dialogbox').style.display = "none";
-      document.getElementById('dialogoverlay').style.display = "none";
-      document.location.reload(true)
+});
+
+
+// Custom Alert Class
+function CustomAlert() {
+
+    this.alert = function (message, title) {
+
+        document.body.innerHTML +=
+            '<div id="dialogoverlay"></div>' +
+            '<div id="dialogbox" class="slit-in-vertical">' +
+            '<div>' +
+            '<div id="dialogboxhead"></div>' +
+            '<div id="dialogboxbody"></div>' +
+            '<div id="dialogboxfoot"></div>' +
+            '</div></div>';
+
+        let dialogoverlay = document.getElementById('dialogoverlay');
+        let dialogbox = document.getElementById('dialogbox');
+
+        let winH = window.innerHeight;
+        dialogoverlay.style.height = winH + "px";
+
+        dialogbox.style.top = "100px";
+
+        dialogoverlay.style.display = "block";
+        dialogbox.style.display = "block";
+
+        if (typeof title === 'undefined') {
+            document.getElementById('dialogboxhead').style.display = 'none';
+        } else {
+            document.getElementById('dialogboxhead').innerHTML =
+                '<i class="fa fa-exclamation-circle"></i> ' + title;
+        }
+
+        document.getElementById('dialogboxbody').innerHTML = message;
+
+        document.getElementById('dialogboxfoot').innerHTML =
+            '<button onclick="customAlert.ok()">OK</button>';
     }
-  }
-  
-  let customAlert = new CustomAlert();
+
+    this.ok = function () {
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+        location.reload();
+    }
+}
+
+
+// Create Alert Object
+let customAlert = new CustomAlert();
